4f5b9c8ad9585721f1188990c2f4d912039e666c
lzx <123456789>
2019-01-20 18:10:50 +0800

    1

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # coding=utf8
 import itchat
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 itchat.auto_login()
 
-itchat.send('Hello, filehelper', toUserName='filehelper')
+sched = BlockingScheduler()
+
+
+def my_job():
+    itchat.send('Hello, filehelper', toUserName='filehelper')
+
+
+sched.add_job(my_job, 'cron', hour='18', minute=10)
+sched.start()
+
